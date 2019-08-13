351ea5780a44ccc24e0ddd6afa671db429dbdfab
prakashmuth <prakash9519@gmail.com>
2019-08-13 12:54:30 +0530

    Stream Api

--- LambdaExpression/src/chapter1/FilteringApple.java
+++ LambdaExpression/src/chapter1/FilteringApple.java
@@ -3,6 +3,8 @@ package chapter1;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class FilteringApple
@@ -12,11 +14,18 @@ public class FilteringApple
         List<Apple> apples= Arrays.asList(
                 new Apple("green",80),
                 new Apple("blue",120),
-                new Apple("green",60)
+                new Apple("green",60),
+                new Apple("yellow",200)
         );
 
         apples.stream()
                 .filter(apple -> apple.getColour().equals("green"))
                 .forEach(apple -> System.out.println(apple.getWeight()));
+
+        //sort apple
+        //Collections.sort(apples,(a1,a2) -> a1.getColour().compareTo(a2.getColour()));
+        apples.sort(Comparator.comparing(Apple::getColour).reversed().thenComparing(Apple::getWeight));
+
+        System.out.println(apples);
     }
 }
